34fd1c970feb27a33edbb6aaa81a7389b74c7781
maliByatzes <malib2027@gmail.com>
2024-06-19 10:58:45 +0200

    implement the rest of auth handlers

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import argon2 from "argon2";
 import { z } from 'zod';
+import { generateToken } from "../utils/generateToken.js";
 
 const signupBody = z.object({
   username: z.string().min(3).max(30),
@@ -54,6 +55,55 @@ export const signup = async (req, res) => {
   }
 };
 
-export const login = async (req, res) => {};
+const loginBody = z.object({
+  username: z.string().min(3).max(30),
+  password: z.string().min(6),
+});
 
-export const logout = async (req, res) => {};
+export const login = async (req, res) => {
+  const { error } = loginBody.safeParse(req.body);
+
+  if (error) {
+    return res.status(400).json({ error: error.message });
+  }
+
+  const { username, password } = req.body;
+
+  try {
+    if (!username || !password) {
+      return res.status(400).json({ error: "Missing required fields" });
+    }
+
+    const user = await User.findOne({ username });
+    const validPassword = await argon2.verify(user.password, password);
+
+    if (!user || !validPassword) {
+      return res.status(400).json({ error: "Invalid username or password" });
+    }
+
+    await generateToken(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName,
+    });
+  } catch (err) {
+    console.log(`Error in login: ${err.message}`);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    res.cookie("jwt", "", {
+      maxAge: 0,
+    });
+
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (err) {
+    console.log(`Error in logout: ${err.message}`);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 import connectToMongoDB from './db/connectToMongoDB.js';
 import authRoutes from './routes/auth.routes.js';
@@ -12,6 +13,7 @@ const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/v1/auth', authRoutes);
 
