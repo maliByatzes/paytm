e37fb04117ca96e852d1fd8422a9845135a33b78
maliByatzes <malib2027@gmail.com>
2024-06-20 10:38:02 +0200

    implement transfer money handler

--- backend/controllers/account.controllers.js
+++ backend/controllers/account.controllers.js
@@ -1,5 +1,6 @@
 import User from '../models/user.model.js';
 import Account from '../models/account.model.js';
+import mongoose from 'mongoose';
 
 export const getBalance = async (req, res) => {
   const userId = req.user._id;
@@ -18,6 +19,52 @@ export const getBalance = async (req, res) => {
     return res.status(200).json({ balance: account.balance });
   } catch (err) {
     console.log(`Error in getBalance: ${err.message}`);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const transferMoney = async (req, res) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  const { id: receiverId } = req.params;
+  const { amount } = req.body;
+  const userId = req.user._id;
+
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const account = await Account.findOne({ userId: user._id });
+    if (!account) {
+      return res.status(404).json({ error: "Account not found" });
+    }
+
+    const receiverAccount = await Account.findOne({ userId: receiverId });
+    if (!receiverAccount) {
+      return res.status(404).json({ error: "Receiver Account not found" });
+    }
+
+    if (account.balance < amount) {
+      return res.status(400).json({ error: "Insufficient balance" });
+    }
+
+    account.balance -= amount;
+    receiverAccount.balance += amount;
+
+    await account.save({ session });
+    await receiverAccount.save({ session });
+
+    await session.commitTransaction();
+    session.endSession();
+
+    return res.status(200).json({ message: "Money transferred successfully" });
+  } catch (err) {
+    await session.abortTransaction();
+    session.endSession();
+    console.log(`Error in transferMoney: ${err.message}`);
     res.status(500).json({ error: "Internal server error" });
   }
 };
--- backend/routes/account.routes.js
+++ backend/routes/account.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { protectRoute } from '../middleware/protectRoute.js';
-import { getBalance } from '../controllers/account.controllers.js';
+import { getBalance, transferMoney } from '../controllers/account.controllers.js';
 
 const router = express.Router();
 
 router.get('/balance', protectRoute, getBalance);
+router.post('/transfer/:id', protectRoute, transferMoney);
 
 export default router;
