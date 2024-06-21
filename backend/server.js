6356c1f914b17da2688101f150a26ba5f735933a
maliByatzes <malib2027@gmail.com>
2024-06-21 14:46:29 +0200

    setup application for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 import connectToMongoDB from './db/connectToMongoDB.js';
 import authRoutes from './routes/auth.routes.js';
@@ -13,6 +14,8 @@ const app = express();
 
 const PORT = process.env.PORT || 8000;
 
+const __dirname = path.resolve();
+
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
@@ -20,6 +23,12 @@ app.use(cookieParser());
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/accounts', accountRoutes);
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
